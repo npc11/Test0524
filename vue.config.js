0cd42653065cfb586e6da87a93c235401d598fce
npc11 <dhwkfn55@gmail.com>
2022-05-26 12:45:37 +0900

    Changes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  devServer: {
+    proxy: {
+      'api': {
+        target: 'http://localhost:9000',
+        ws: true,
+        changeOrigin: true
+      },
+    }
+  }
+}
